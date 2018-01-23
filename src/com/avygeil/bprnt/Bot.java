87957da2df2113e507db0b5faccd6fe0dbfd8541
Avygeil <avyg31l@gmail.com>
2018-01-24 00:20:32 +0100

    discord4j

--- src/com/avygeil/bprnt/Bot.java
+++ src/com/avygeil/bprnt/Bot.java
@@ -1,8 +1,38 @@
 package com.avygeil.bprnt;
 
+import sx.blah.discord.api.ClientBuilder;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.util.DiscordException;
+
 public class Bot {
 
 	public static void main(String[] args) {
+		if (args.length < 1) {
+			throw new IllegalArgumentException();
+		}
+		
+		IDiscordClient client = null;
+		
+		try {
+			ClientBuilder builder = new ClientBuilder();
+			builder.withToken(args[0]);
+			client = builder.login();
+		} catch (DiscordException e) {
+			e.printStackTrace();
+		}
+		
+		if (client != null) {
+			new Bot(client).Run();
+		}
+	}
+	
+	private IDiscordClient client;
+	
+	public Bot(IDiscordClient client) {
+		this.client = client;
+	}
+	
+	public void Run() {
 		System.out.println("hello world");
 	}
 
